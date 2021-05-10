576e3e3f748488a2fc3f25fce532d6d4b3f64b25
Eric Hunter <ehunter7@live.com>
2021-05-10 08:08:19 -0700

    add styling for new puckup form

--- client/src/components/NewPU.js
+++ client/src/components/NewPU.js
@@ -1,46 +1,73 @@
-
-const NewPU = ({handleInput, handleSubmit}) => {
-
-
+const NewPU = ({ handleInput, handleSubmit }) => {
   return (
-    <div>
+    <div className="newPUForm">
       <form>
-        <label htmlFor="pro">PRO</label>
-        <input
-          id="pro"
-          type="number"
-          name="pro"
-          required
-          onChange={(e) => handleInput(e)}
-        ></input>
-        <label htmlFor="carrier">Carrier</label>
-        <input
-          id="carrier"
-          type="text"
-          name="carrier"
-          required
-          onChange={(e) => handleInput(e)}
-        ></input>
-        <label htmlFor="puDate">Pick-up Date</label>
-        <input
-          id="puDate"
-          type="date"
-          name="puDate"
-          required
-          onChange={(e) => handleInput(e)}
-        ></input>
-        <label htmlFor="puTime">Pick-up Time</label>
-        <input
-          id="puTime"
-          type="text"
-          name="puTime"
-          defaultValue="0800 to 1300"
-          onChange={(e) => handleInput(e)}
-        ></input>
-        <label htmlFor="notes">Notes</label>
-          <textarea id="notes" name="notes" onChange={(e) => handleInput(e)}></textarea>
-        
-        <button onClick={(e) => handleSubmit(e)}>submit</button>
+        <div className="row">
+          <div className="col-md-3">
+            <label htmlFor="pro">PRO</label>
+            <input
+              className="newPUInput"
+              id="pro"
+              type="number"
+              name="pro"
+              required
+              onChange={(e) => handleInput(e)}
+            ></input>
+          </div>
+
+          <div className="col-md-3">
+            <label htmlFor="carrier">Carrier</label>
+            <input
+              className="newPUInput"
+              id="carrier"
+              type="text"
+              name="carrier"
+              required
+              onChange={(e) => handleInput(e)}
+            ></input>
+          </div>
+
+          <div className="col-md-3">
+            <label htmlFor="puDate">pickup Date</label>
+            <input
+              className="newPUInput"
+              id="puDate"
+              type="date"
+              name="puDate"
+              required
+              onChange={(e) => handleInput(e)}
+            ></input>
+          </div>
+
+          <div className="col-md-3">
+            <label htmlFor="puTime">pickup Time</label>
+            <input
+              className="newPUInput"
+              id="puTime"
+              type="text"
+              name="puTime"
+              defaultValue="0800 to 1300"
+              onChange={(e) => handleInput(e)}
+            ></input>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-md-10">
+            <label htmlFor="notes">Notes</label>
+            <textarea
+              id="notes"
+              className="notesTextArea"
+              name="notes"
+              onChange={(e) => handleInput(e)}
+            ></textarea>
+          </div>
+          <div className="col-md-2 puButton">
+            <button className="newPUButton " onClick={(e) => handleSubmit(e)}>
+              Submit
+            </button>
+          </div>
+        </div>
       </form>
     </div>
   );
--- client/src/components/Selection.js
+++ client/src/components/Selection.js
@@ -6,36 +6,40 @@ const Selection = ({ newPU, showNewPU, setCompletedPage, completedPage }) => {
   let history = useHistory();
 
   const handleClick = (e) => {
-
     setCompletedPage(!completedPage);
 
-    if(e.target.name === "completed"){
-      history.push("/completed");      
+    if (e.target.name === "completed") {
+      history.push("/completed");
     } else {
       history.push("/pending");
-    };
+    }
   };
 
   return (
     <div className="selectionGroup">
       <div className="selectionButtons">
-{ !completedPage ? <><button className="selcBTN" onClick={() => showNewPU(!newPU)}>
-          Create New Pick-up
-        </button>
-
-        <button
-          className="completedButton selcBTN"
-          name='completed'
-          onClick={(e) => handleClick(e)}
-        >
-          Completed
-        </button> </>:         <button
-          className="completedButton selcBTN"
-          name="pending"
-          onClick={(e) => handleClick(e)}
-        >
-          Pending
-        </button>}
+        {!completedPage ? (
+          <>
+            <button className="selcBTN" onClick={() => showNewPU(!newPU)}>
+              Create New Pick-up
+            </button>
+            <button
+              className="completedButton selcBTN"
+              name="completed"
+              onClick={(e) => handleClick(e)}
+            >
+              Completed
+            </button>{" "}
+          </>
+        ) : (
+          <button
+            className="completedButton selcBTN"
+            name="pending"
+            onClick={(e) => handleClick(e)}
+          >
+            Pending
+          </button>
+        )}
       </div>
     </div>
   );
