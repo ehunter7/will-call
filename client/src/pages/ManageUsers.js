144de26bbf51beba5774b5241f568ea61c8191b3
Eric Hunter <ehunter7@live.com>
2021-05-19 07:21:27 -0700

    add manage comonent

---
+++ client/src/pages/ManageUsers.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Card, Button, Form } from "react-bootstrap";
+
+const ManageUsers = () => {
+  return (
+    <>
+      <Container className="d-flex justify-content-center align-items mt-5">
+        <Card></Card>
+      </Container>
+    </>
+  );
+};
+
+export default ManageUsers;
